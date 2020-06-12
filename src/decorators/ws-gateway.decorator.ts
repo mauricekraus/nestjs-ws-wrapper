a4446c18c2523df0ac61403412a7f32ad43eb7c3
Maurice Kraus <maurice.kraus@stud.tu-darmstadt.de>
2020-06-12 21:59:32 +0200

    Fix gateway decorator visibility

--- src/decorators/ws-gateway.decorator.ts
+++ src/decorators/ws-gateway.decorator.ts
@@ -3,6 +3,5 @@ import { Metakeys } from './meta-keys.enum';
 export function WSGateway(path: string): ClassDecorator {
   return (target) => {
     Reflect.defineMetadata(Metakeys.GatewayPath, path, target);
-    return target;
   };
 }
